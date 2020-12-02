8d27383d572a7bbfe385d15792b042f884663a5c
Houcem Ben Amor Chlegou <nicolastsue@gmail.com>
2020-12-02 11:31:00 +0100

    add member service + database.json

---
+++ src/services/member.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import {GLOBAL} from '../app/app-config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MemberService {
+  public placeholderMembers: any[] = GLOBAL._DB.members;
+
+  constructor() { }
+}
